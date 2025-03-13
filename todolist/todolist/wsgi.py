0dde6a2e301d90c38dd635b66142ec515ae2783c
Giselle <nunesgiselle@bol.com.br>
2025-03-13 00:28:13 -0300

    Adicionar .gitignore e remover arquivos desnecessários do controle de versão

--- todolist/todolist/wsgi.py
+++
@@ -1,7 +0,0 @@
-import os
-from django.core.wsgi import get_wsgi_application
-
-# Certifique-se de substituir 'todolist' pelo nome correto do seu projeto
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'todolist.settings')
-
-application = get_wsgi_application()
